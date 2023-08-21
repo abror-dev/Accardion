24c81b1805645ba006046527d4f00cdef377ab0d
abror2007-ux <abrorrustamov446@gmail.com>
2023-08-21 22:01:00 +0500

    homework day

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var elFaq = document.querySelector('.faq')
+var elFaq1 = document.querySelector('.faq1')
+var elFaq2 = document.querySelector('.faq2')
+function fn() {
+    elFaq.classList.toggle('active')
+
+
+}
+
+function fn1() {
+    elFaq1.classList.toggle('active1')
+}
+
+function fn2() {
+    elFaq2.classList.toggle('active2')
+}
+
+
